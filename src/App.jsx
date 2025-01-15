74ed8dd15c578cd794e7d520f4918d23a75d7389
rubina02akter <rubinaakter0108@gmail.com>
2025-01-15 20:06:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className="text-4xl text-red-900">hello</h2>
+        
+    </>
+  )
+}
+
+export default App
